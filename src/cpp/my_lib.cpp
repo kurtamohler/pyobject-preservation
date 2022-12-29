652ea624796da5d23e5bdced68e0ba220ae50592
Kurt Mohler <kurtamohler@gmail.com>
2022-12-29 11:27:39 -0800

    Create separate C++ library that gets linked in to CPython build

---
+++ src/cpp/my_lib.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "my_lib.h"
+
+MyClass::MyClass() {
+  std::cout << "in MyClass::MyClass()" << std::endl;
+}
---
+++ src/my_lib_py.cpp
@@ -0,0 +1,22 @@
+#include <Python.h>
+#include <iostream>
+
+#include "cpp/my_lib.h"
+
+static PyMethodDef module_methods[] = {
+  { NULL, NULL }
+};
+
+static struct PyModuleDef module = {
+  PyModuleDef_HEAD_INIT,
+  .m_name = "_my_lib",
+  .m_methods = module_methods
+};
+
+PyMODINIT_FUNC
+PyInit__my_lib(void)
+{
+  std::cout << "in PyInit__my_lib()" << std::endl;
+  MyClass();
+  return PyModuleDef_Init(&module);
+}
